fe57d5bbcbb48eb348d33afd175a2846476fcf69
P. Anand Kumar <anandkumarp093@gmail.com>
2024-08-22 10:16:56 -0400

    Add files via upload

---
+++ Auction_System4-B/src/Com/Auction_System/bidder.java
@@ -0,0 +1,18 @@
+package Com.Auction_System;
+
+public class bidder implements observer {
+    private String name;
+
+    public bidder(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String event) {
+        System.out.println(name + " received update: " + event);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Auction_System4-B/src/Com/Auction_System/observer.java
@@ -0,0 +1,6 @@
+package Com.Auction_System;
+
+public interface observer {
+ void update(String event);
+ 
+}
